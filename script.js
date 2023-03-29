ffe5013bc5aaed52c3490bbb44e7f0032bc75318
antsolutionsindia <antsolutionsindia@gmail.com>
2023-03-29 16:02:38 +0530

    Feature additions
    Added multiple Panorama scenes.
    Added Home button

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-var myElement
+var myElement,container,bar
 
 myElement = document.querySelector( '.myDiv' );
-// Create panorama
-const panorama = new PANOLENS.ImagePanorama("https://images.unsplash.com/photo-1557971370-e7298ee473fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3520&q=80");
+bar = document.querySelector( '#bar' );
+
+//Add custom function for progress upate
+function onProgressUpdate ( event ) {
+  var percentage = event.progress.loaded/ event.progress.total * 100;
+  bar.style.width = percentage + "%";
+  if (percentage >= 100){
+    bar.classList.add( 'hide' );
+    setTimeout(function(){
+      bar.style.width = 0;
+    }, 1000);
+  }
+}
+
+// Create panorama1
+const panorama1 = new PANOLENS.ImagePanorama("https://images.unsplash.com/photo-1557971370-e7298ee473fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3520&q=80");
+panorama1.addEventListener( 'progress', onProgressUpdate );
+
+//Create Pano2
+const panorama2 = new PANOLENS.ImagePanorama( 'https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/sunset.jpg' );
+panorama2.addEventListener( 'progress', onProgressUpdate );
+
+
+//Create Pano3
+const panorama3 = new PANOLENS.ImagePanorama( 'https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/road.jpg' );
+panorama3.addEventListener( 'progress', onProgressUpdate );
+
+container = document.querySelector( '#panolens-container' );
 
 // Create viewer
-const viewer = new PANOLENS.Viewer({
-  container: document.querySelector("#panolens-container"),
-});
+const viewer = new PANOLENS.Viewer({ container: container });
 //viewer.OrbitControls.noZoom = true;
 viewer.addViewIndicator()
 viewer.setCameraFov(100)
-//viewer.enableControl()
 
 
 // Add panorama to viewer
-viewer.add(panorama);
+viewer.add(panorama1);
 
+//Add second and third pano
+viewer.add( panorama1, panorama2, panorama3 );
 
 // Create infospot
 const infospot = new PANOLENS.Infospot(100, PANOLENS.DataImage.Info,false);
@@ -24,8 +49,40 @@ infospot.material.opacity = 0.5 ;
 infospot.position.set(1500, -300, -800);
 infospot.addHoverElement(myElement, 170);
 
-viewer.getControl()
+
+//Add Home button
+var controlItemPoster = {
+  style: {
+    backgroundImage: 'url(https://cdn-icons-png.flaticon.com/512/25/25694.png)',
+    float: 'left'
+  },
+  
+  onTap: function(){
+    infospot.focus();
+  }
+};
+viewer.appendControlItem(controlItemPoster);
+
 
 // Add infospot to panorama
-panorama.add(infospot);
+panorama1.add(infospot);
 
+
+
+// Maunal Set Panorama
+var button1 = document.querySelector( '#btn1' );
+var button2 = document.querySelector( '#btn2' );
+var button3 = document.querySelector( '#btn3' );
+
+
+// Enter panorama when load completes
+function onButtonClick( targetPanorama ) {
+  bar.classList.remove( 'hide' );
+  viewer.setPanorama( targetPanorama );
+}
+
+button1.addEventListener( 'click', onButtonClick.bind( this, panorama1 ) );
+
+button2.addEventListener( 'click', onButtonClick.bind( this, panorama2 ) );
+
+button3.addEventListener( 'click', onButtonClick.bind( this, panorama3 ) );
